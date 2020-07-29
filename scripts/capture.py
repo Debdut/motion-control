102822210647b3d20bf1e8cc1a23b65f70e18a10
Debdut Karmakar <iawaiponly@gmail.com>
2020-07-30 03:48:31 +0530

    Nose Detection

---
+++ scripts/capture.py
@@ -0,0 +1,11 @@
+import cv2
+
+capture = cv2.VideoCapture(0)
+while capture.isOpened():
+  ret, frame = capture.read()
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  cv2.imshow('frame', gray)
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+capture.release()
+cv2.destroyAllWindows()
